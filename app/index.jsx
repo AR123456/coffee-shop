e9b9c245b90f0e952aafeea0ebac7213648b2f48
AR123456 <coloradoz99@gmail.com>
2026-06-28 06:55:07 -0400

    moving explore

---
+++ app/index.jsx
@@ -0,0 +1,37 @@
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
+
+import icedCoffeeImg from "@/assets/images/iced-coffee.png";
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={icedCoffeeImg} style={styles.image}>
+        <Text style={styles.text}>Coffee Shop</Text>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default app;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    resizeMode: "cover",
+    width: "100%",
+    height: "100%",
+  },
+});
