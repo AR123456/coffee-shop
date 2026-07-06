265ec48c77aabed761f9975041bcb303faf634fa
AR123456 <coloradoz99@gmail.com>
2026-07-06 05:43:20 -0400

    adding use effect to handle loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,6 +17,15 @@ export default function RootLayout() {
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
