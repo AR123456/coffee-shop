b8f21600319f20f4d16b42df899d3badc4d38f4a
AR123456 <coloradoz99@gmail.com>
2026-07-08 05:08:07 -0400

    mobile scrollview web safearea

--- app/menu.jsx
+++ app/menu.jsx
@@ -1,5 +1,6 @@
 import { Appearance, StyleSheet, Platform, SafeAreaView } from "react-native";
 import { Colors } from "@/constants/Colors";
+import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
 
 export default function MenuScreen() {
   const colorScheme = Appearance.getColorScheme();
@@ -7,7 +8,7 @@ export default function MenuScreen() {
   // styles function
   const styles = createStyles(theme, colorScheme);
   // define containter for app web vs mobile to render flat list -need imports
-  const Container = (Platform.OS = "");
+  const Container = Platform.OS === "web" ? ScrollView : SafeAreaView;
 }
 
 function createStyles(theme, colorScheme) {
