e4db3565a9959e1e24fc807e42d13a3dcaf7699f
AR123456 <coloradoz99@gmail.com>
2026-07-16 06:00:42 -0400

    contact

--- app/index.jsx
+++ app/index.jsx
@@ -19,9 +19,9 @@ const app = () => {
             <Text style={styles.buttonText}>Contact Us</Text>
           </Pressable>
         </Link>
-        <Link href="/menu" style={{ marginHorizontal: "auto" }} asChild>
-          <Pressable style={styles.button}>
-            <Text style={styles.buttonText}>Menu</Text>
+        <Link href="/menu" asChild>
+          <Pressable>
+            <Text>Menu</Text>
           </Pressable>
         </Link>
       </ImageBackground>
@@ -55,12 +55,10 @@ const styles = StyleSheet.create({
   },
   button: {
     height: 60,
-    width: 150,
     borderRadius: 20,
     justifyContent: "center",
     backgroundColor: "rgba(0,0,0,0.75)",
     padding: 6,
-    marginBottom: 50,
   },
   buttonText: {
     color: "white",
